2c318837c4398f1ef68fda5c6bec459181ae1241
Melanie Williams <melaniep518@gmail.com>
2017-08-04 13:16:13 -0500

    final 2.0

---
+++ script.js
@@ -0,0 +1,159 @@
+function kidFriendly() {
+
+    var tangled_scene = document.getElementById('start_page');
+    tangled_scene.setAttribute('visible', 'false');
+    tangled_scene.setAttribute('position', '-2 30 -0');
+
+    var tangled_scene2 = document.getElementById('scene1_tangled');
+    tangled_scene2.setAttribute('visible', 'false');
+    tangled_scene2.setAttribute('position', "-2 30 -0")
+
+    var tangled_scene1 = document.getElementById('intro_tangled');
+    tangled_scene1.setAttribute('position', '-0.7 2 -3')
+    tangled_scene1.setAttribute('visible', 'true');
+
+
+
+    // var reset = document.getElementById('yellow_right');
+    // reset.setAttribute('onClick', '');
+    // reset.setAttribute('position', '-2 30 -0');
+
+    // var reset_2 = document.getElementById('yellow_left');
+    // reset_2.setAttribute('onClick', ' ');
+    // reset_2.setAttribute('position', '-2 30 -0');
+}
+
+function green_left_tangled() {
+
+    var tangled_scene2 = document.getElementById('intro_tangled');
+    tangled_scene2.setAttribute('visible', 'false');
+    tangled_scene2.setAttribute('position', "-2 30 -0")
+
+    var tangled_scene4 = document.getElementById('scene1_tangled');
+    tangled_scene4.setAttribute('visible', 'false');
+    tangled_scene4.setAttribute('position', "-2 30 -0")
+
+    var tangled_scene3 = document.getElementById('scene1_tangled');
+    tangled_scene3.setAttribute('position', "-0.7 2 -3")
+    tangled_scene3.setAttribute('visible', 'true');
+
+    // var reset_4 = document.getElementById('green_left');
+    // reset_4.setAttribute('onClick', ' ');
+    // reset_4.setAttribute('position', '-2 30 -0');
+
+    // var reset_5 = document.getElementById('green_right');
+    // reset_5.setAttribute('onClick', ' ');
+    // reset_5.setAttribute('position', '-2 30 -0');
+}
+
+function green_right_tangled() {
+
+    var tangled_scene4 = document.getElementById('intro_tangled');
+    tangled_scene4.setAttribute('position', "-2 30 -0")
+    tangled_scene4.setAttribute('visible', 'false');
+
+    var tangled_scene5 = document.getElementById('scene1_tangled');
+    tangled_scene5.setAttribute('position', '-0.7 2 -3')
+    tangled_scene5.setAttribute('visible', 'true');
+
+    // var reset_6 = document.getElementById('green_left');
+    // reset_6.setAttribute('onClick', ' ');
+    // reset_6.setAttribute('position', '-2 30 -0');
+
+    // var reset_7 = document.getElementById('green_right');
+    // reset_7.setAttribute('onClick', ' ');
+    // reset_7.setAttribute('position', '-2 30 -0');
+
+}
+
+function blue_left_tangled() {
+
+    var tangled_scene6 = document.getElementById('intro_tangled');
+    tangled_scene6.setAttribute('visible', 'false');
+    tangled_scene6.setAttribute('position', '-2 30 -0');
+
+    var tangled_scene2 = document.getElementById('scene1_tangled');
+    tangled_scene2.setAttribute('visible', 'false');
+    tangled_scene2.setAttribute('position', "-2 30 -0")
+
+    var tangled_scene7 = document.getElementById('scene2_tangled');
+    tangled_scene7.setAttribute('position', '-0.7 2 -3')
+    tangled_scene7.setAttribute('visible', 'true');
+
+    // var reset_8 = document.getElementById('blue_left1');
+    // reset_8.setAttribute('onClick', ' ');
+    // reset_8.setAttribute('position', '-2 30 -0');
+
+    // var reset_9 = document.getElementById('blue_right1');
+    // reset_9.setAttribute('onClick', ' ');
+    // reset_9.setAttribute('position', '-2 30 -0');
+
+}
+
+function purple_right_tangled(){
+    
+    var tangled_scene2 = document.getElementById('scene1_tangled');
+    tangled_scene2.setAttribute('visible', 'false');
+    tangled_scene2.setAttribute('position', "-2 30 -0")
+
+    var tangled_scene8 = document.getElementById('scene2_tangled');
+    tangled_scene8.setAttribute('position', '-2 30 -0');
+    tangled_scene8.setAttribute('visible', 'false');
+
+    var tangled_scene9 = document.getElementById('scene3_tangled');
+    tangled_scene9.setAttribute('position', '-0.7 2 -3');
+    tangled_scene9.setAttribute('visible', 'true');
+    
+    // var reset_10 = document.getElementById('purple_left');
+    // reset_10.setAttribute('onClick', ' ');
+    // reset_10.setAttribute('position', '-2 30 -0');
+
+    // var reset_11 = document.getElementById('purple_right');
+    // reset_11.setAttribute('onClick', ' ');
+    // reset_11.setAttribute('position', '-2 30 -0 ');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ROBBERY FUNCTIONS
+
+function forGrownups() {
+    var robbery_scene = document.getElementById('start_page');
+    robbery_scene.setAttribute('visible', 'false');
+    tangled_scene.setAttribute('position', '-2 30 -0');
+
+
+    var robbery_scene2 = document.getElementById('intro_robbery');
+    robbery_scene2.setAttribute('visible', 'true');
+    tangled_scene.setAttribute('position', '-0.7 2 -3');
+
+
+    // var reset_1 = document.getElementById('yellow_left');
+    // reset_1.setAttribute('onclick', ' ');
+
+    // var reset_3 = document.getElementById('yellow_right');
+    // reset_3.setAttribute('onclick', ' ');
+}
+
+
+// function white_left_robbery() {
+//     var robbery_scene_masks = document.getElementById('white_left')
+//     robbery_scene_masks.setAttribute('visible', 'true');
+
+//     var mask_on_option = document.getElementById('white_left')
+//     mask_on_option.setAttribute('onclick', ' ')
+
+//     var mask_off_option = document.getElementById('white_right')
+//     mask_off_option.setAttribute('onclick', '')
+// }
